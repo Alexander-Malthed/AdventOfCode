c032938e710956bd22e1b5d64a931249aa425360
AlexanderMalthed <Kattenlevererar@gmail.com>
2022-01-28 02:04:11 +0100

    Days 1-15 updated

---
+++ 2021/Day1/Day1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class Day1
+{
+    static void Main(string[] args)
+    {
+        int[] input = Array.ConvertAll(File.ReadAllLines("D:/Code/input.txt"), s => int.Parse(s));
+        int result = 0;
+
+        for (int i = 0; i < input.Length - 3; i++)
+        {
+            if (input[i + 3] > input[i])
+            {
+                result++;
+            }
+        }
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
