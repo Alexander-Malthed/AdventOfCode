c65dd39fe4de6872c235f6ee597c00ecf31eef4c
AlexanderMalthed <Kattenlevererar@gmail.com>
2022-02-16 23:21:22 +0100

    Cleanup

---
+++ 2021/Day15/Day15.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+namespace Day15
+{
+    class Day15
+    {
+        static Node[][] map;
+        static int mapYLenght, mapXLenght;
+        static Heap<Node> open;
+        static HashSet<Node> closed = new HashSet<Node>();
+
+        static void Main(string[] args)
+        {
+            // Construct the Node jagged array
+            int[][] intMap = File.ReadAllLines("D:/AdventOfCode/2021/Day15/input.txt")
+                .Select((l, i) => l
+                    .Select((c, j) => Convert.ToInt32(c.ToString()))
+                    .ToArray())
+                .ToArray();
+            map = ExpandMapForPart2(intMap);
+
+            // Setup
+            mapYLenght = map.Length;
+            mapXLenght = map[0].Length;
+            open = new Heap<Node>(mapYLenght * mapXLenght);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            Node start = map[0][0];
+            Node end = map[mapYLenght - 1][mapXLenght - 1];
+
+            map[0][0].risk = 0;
+            open.Add(map[0][0]);
+            Node current = map[0][0];
+
+            // A* loop finding the cheapest path
+            while (open.Count > 0)
+            {
+                // The heap allows it to efficently find the best step to take.
+                current = open.RemoveFirst();
+                closed.Add(current);
+
+                if (current == end)
+                    break;
+
+                // Add or update valid neighbours.
+                foreach (Node neighbour in GetNeighbours(current))
+                {
+                    if (closed.Contains(neighbour))
+                        continue;
+
+                    int newCostToNeighbour = current.gCost + neighbour.risk;
+                    if (newCostToNeighbour < neighbour.gCost || !open.Contains(neighbour))
+                    {
+                        neighbour.gCost = newCostToNeighbour;
+                        neighbour.hCost = mapYLenght - 1 - neighbour.mapY + mapXLenght - 1 - neighbour.mapX;
+                        neighbour.parent = current;
+
+                        if (!open.Contains(neighbour))
+                            open.Add(neighbour);
+                        else
+                            open.UpdateItem(neighbour);
+                    }
+                }
+            }
+            sw.Stop();
+
+            // Retrace the cheapest path to calculate the result.
+            int totalRisk = current.risk;
+            while (current.parent != start)
+            {
+                current = current.parent;
+                totalRisk += current.risk;
+            }
+
+            File.WriteAllText("D:/AdventOfCode/2021/Day15/result.txt", totalRisk.ToString());
+            Console.WriteLine($"total risk: {totalRisk}");
+            Console.WriteLine($"time: {sw.ElapsedMilliseconds}ms");
+        }
+
+        // Copy the puzzle input to a 5*5 grid of duplicates.
+        // Each step to the right or down in the 5*5 grid adds 1 to each value in that tile.
+        // Incrementing a 9 will loop it back around to a 1.
+        static Node[][] ExpandMapForPart2(int[][] oldMap)
+        {
+            int oldMaxY = oldMap.Length;
+            int oldMaxX = oldMap[0].Length;
+
+            Node[][] newMap = new Node[oldMaxY * 5][];
+            for (int i = 0; i < newMap.Length; i++)
+                newMap[i] = new Node[oldMaxX * 5];
+
+            for (int expandY = 0; expandY < 5; expandY++)
+            {
+                for (int expandX = 0; expandX < 5; expandX++)
+                {
+                    for (int i = 0; i < oldMap.Length; i++)
+                    {
+                        for (int j = 0; j < oldMap[0].Length; j++)
+                        {
+                            int newRisk = (oldMap[i][j] + expandX + expandY);
+                            if (newRisk > 9)
+                                newRisk = (newRisk + 1) % 10;
+
+                            int newY = i + (oldMaxY * expandY);
+                            int newX = j + (oldMaxX * expandX);
+                            newMap[newY][newX] = new Node { risk = newRisk, mapY = newY, mapX = newX };
+                        }
+                    }
+                }
+            }
+
+            return newMap;
+        }
+
+        static List<Node> GetNeighbours(Node node)
+        {
+            List<Node> neighbours = new List<Node>();
+            if (node.mapY + 1 < mapYLenght)
+                neighbours.Add(map[node.mapY + 1][node.mapX]);
+
+            if (node.mapY - 1 >= 0)
+                neighbours.Add(map[node.mapY - 1][node.mapX]);
+
+            if (node.mapX + 1 < mapXLenght)
+                neighbours.Add(map[node.mapY][node.mapX + 1]);
+
+            if (node.mapX - 1 >= 0)
+                neighbours.Add(map[node.mapY][node.mapX - 1]);
+
+            return neighbours;
+        }
+
+        static void PrintPath(Node start, Node end)
+        {
+            List<Node> path = new List<Node>();
+            Node current = end;
+
+            while (current != start)
+            {
+                path.Add(current);
+                current = current.parent;
+            }
+            path.Reverse();
+
+            foreach (Node node in path)
+            {
+                Console.WriteLine(node.risk);
+            }
+        }
+    }
+}
---
+++ 2021/Day18/Reducer.cs
@@ -0,0 +1,279 @@
+﻿using System;
+
+namespace Day18
+{
+    static class Reducer
+    {
+        static Heap<Pair> explosionOrder;
+        static Heap<RegularNumber> splitOrder;
+
+        static public void SetExplosionAndSplitOrder(Heap<Pair> newExplosionOrder, Heap<RegularNumber> newSplitOrder)
+        {
+            explosionOrder = newExplosionOrder;
+            splitOrder = newSplitOrder;
+        }
+
+        static public Pair ReducePair(Pair pairToReduce)
+        {
+            explosionOrder = new Heap<Pair>(100);
+            splitOrder = new Heap<RegularNumber>(100);
+
+            FindAllPairsToExplode(pairToReduce, 4);
+
+            while (explosionOrder.Count > 0 || splitOrder.Count > 0)
+            {
+                if (explosionOrder.Count > 0)
+                {
+                    Pair pairToExplode = explosionOrder.RemoveFirst();
+
+                    //Console.WriteLine($"exploded [{((RegularNumber)pairToExplode.LeftChild).Value},{((RegularNumber)pairToExplode.RightChild).Value}]");
+                    ExplodePair(pairToExplode);
+                    //PrintString(pairToReduce);
+                    continue;
+                }
+
+                if (splitOrder.Count > 0)
+                {
+                    RegularNumber numberToSplit = splitOrder.RemoveFirst();
+
+                    if (!numberToSplit.Valid)
+                    {
+                        continue;
+                    }
+
+                    //Console.WriteLine($"splitted [{numberToSplit.Value}]");
+                    SplitRegularNumber(numberToSplit);
+                    //PrintString(pairToReduce);
+                }
+            }
+
+            return pairToReduce;
+        }
+
+        #region Actions
+        // Add the left value of the pair into the immediate number to the left.
+        // Add the right value of the pair into the immediate number to the right.
+        // Replace pair with a RegularNumber with the value 0.
+        // Exploding [1,2] in [3,[[1,2],4]] results in [4,[0,6]].
+        static void ExplodePair(Pair explodingPair)
+        {
+            // Add to right number. Check if that number should be split.
+            RegularNumber closestNumber = FindClosestRegularNumberRight(explodingPair);
+            if (closestNumber != null)
+            {
+                closestNumber.Value += ((RegularNumber)explodingPair.RightChild).Value;
+
+                if (closestNumber.Value > 9)
+                {
+                    closestNumber.UpdateDistance();
+
+                    if (!splitOrder.Contains(closestNumber))
+                    {
+                        splitOrder.Add(closestNumber);
+                    }
+                }
+            }
+
+            // Add to left number. Check if that number should be split.
+            closestNumber = FindClosestRegularNumberLeft(explodingPair);
+            if (closestNumber != null)
+            {
+                closestNumber.Value += ((RegularNumber)explodingPair.LeftChild).Value;
+
+                if (closestNumber.Value > 9)
+                {
+                    closestNumber.UpdateDistance();
+
+                    if (!splitOrder.Contains(closestNumber))
+                    {
+                        splitOrder.Add(closestNumber);
+                    }
+                }
+            }
+
+            // Replace the pair with a 0.
+            new RegularNumber(0, explodingPair.ParentPair, explodingPair.ChildState);
+
+            explodingPair.RightChild.Valid = false;
+            explodingPair.LeftChild.Valid = false;
+            explodingPair = null;
+        }
+
+        // Numbers above 9 gets split into a pair following the rule [floor(n/2), ceil(n/2)].
+        // 15 becomes [7,8].
+        static void SplitRegularNumber(RegularNumber numberToExplode)
+        {
+            // Create pair.
+            int leftValue = (int)Math.Floor((float)numberToExplode.Value / 2);
+            int rightValue = (int)Math.Ceiling((float)numberToExplode.Value / 2);
+            Pair newPair = new Pair(leftValue, rightValue, numberToExplode.ParentPair, numberToExplode.ChildState);
+
+            // Check if the new pair should explode.
+            Pair parent = newPair.ParentPair;
+            int numOfParents = 1;
+            while (parent.ParentPair != null)
+            {
+                parent = parent.ParentPair;
+                if (++numOfParents == 4)
+                {
+                    newPair.UpdateDistance();
+
+                    if (!explosionOrder.Contains(newPair))
+                    {
+                        explosionOrder.Add(newPair);
+                    }
+
+                    break;
+                }
+            }
+
+            // Check if the new numbers should be split.
+            RegularNumber leftNumber = (RegularNumber)newPair.LeftChild;
+            RegularNumber rightNumber = (RegularNumber)newPair.RightChild;
+
+            if (leftNumber.Value > 9)
+            {
+                splitOrder.Add(leftNumber);
+            }
+
+            if (rightNumber.Value > 9)
+            {
+                splitOrder.Add(rightNumber);
+            }
+        }
+        #endregion
+
+        #region Find
+        #region FindClosestNumber
+        static RegularNumber FindClosestRegularNumberRight(Pair explodingPair)
+        {
+            Pair parent = explodingPair.ParentPair;
+            Pair curSearchPair;
+
+            if (explodingPair.ChildState == ChildState.LEFTCHILD)
+            {
+                if (parent.RightChild is RegularNumber)
+                {
+                    return (RegularNumber)parent.RightChild;
+                }
+
+                curSearchPair = (Pair)parent.RightChild;
+            }
+            else
+            {
+                while (true)
+                {
+                    switch (parent.ChildState)
+                    {
+                        case ChildState.NONE:
+                            return null;
+
+                        case ChildState.LEFTCHILD:
+                            break;
+
+                        case ChildState.RIGHTCHILD:
+                            parent = parent.ParentPair;
+                            continue;
+
+                        default:
+                            break;
+                    }
+
+                    break;
+                }
+
+                if (parent.ParentPair.RightChild is RegularNumber)
+                {
+                    return (RegularNumber)parent.ParentPair.RightChild;
+                }
+
+                curSearchPair = (Pair)parent.ParentPair.RightChild;
+            }
+
+            while (curSearchPair.LeftChild is Pair)
+            {
+                curSearchPair = (Pair)curSearchPair.LeftChild;
+            }
+
+            return (RegularNumber)curSearchPair.LeftChild;
+        }
+
+        static RegularNumber FindClosestRegularNumberLeft(Pair explodingPair)
+        {
+            Pair parent = explodingPair.ParentPair;
+            Pair curSearchPair;
+
+            if (explodingPair.ChildState == ChildState.RIGHTCHILD)
+            {
+                if (parent.LeftChild is RegularNumber)
+                {
+                    return (RegularNumber)parent.LeftChild;
+                }
+
+                curSearchPair = (Pair)parent.LeftChild;
+            }
+            else
+            {
+                while (true)
+                {
+                    switch (parent.ChildState)
+                    {
+                        case ChildState.NONE:
+                            return null;
+
+                        case ChildState.LEFTCHILD:
+                            parent = parent.ParentPair;
+                            continue;
+
+                        case ChildState.RIGHTCHILD:
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    break;
+                }
+
+                if (parent.ParentPair.LeftChild is RegularNumber)
+                {
+                    return (RegularNumber)parent.ParentPair.LeftChild;
+                }
+
+                curSearchPair = (Pair)parent.ParentPair.LeftChild;
+            }
+
+            while (curSearchPair.RightChild is Pair)
+            {
+                curSearchPair = (Pair)curSearchPair.RightChild;
+            }
+
+            return (RegularNumber)curSearchPair.RightChild;
+        }
+        #endregion
+
+        static void FindAllPairsToExplode(Pair pair, int depth)
+        {
+            if (depth <= 0)
+            {
+                if (pair.LeftChild is RegularNumber && pair.RightChild is RegularNumber)
+                {
+                    pair.UpdateDistance();
+                    explosionOrder.Add(pair);
+                    return;
+                }
+            }
+
+            if (pair.RightChild is Pair)
+            {
+                FindAllPairsToExplode((Pair)pair.RightChild, depth - 1);
+            }
+
+            if (pair.LeftChild is Pair)
+            {
+                FindAllPairsToExplode((Pair)pair.LeftChild, depth - 1);
+            }
+        }
+        #endregion
+    }
+}
