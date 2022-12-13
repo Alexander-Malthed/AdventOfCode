10c18a6977c0e73d04e64074c3285c9d688aa024
AlexanderMalthed <Kattenlevererar@gmail.com>
2022-12-13 22:06:35 +0100

    Day 11

---
+++ 2022/Day11/Monkey.cs
@@ -0,0 +1,92 @@
+﻿namespace Day11 {
+    internal class Monkey {
+        public Queue<ulong> items;
+        readonly OperationType operation;
+        readonly ulong operationNumber;
+        public ulong TestNumber { get; private set; }
+        readonly Dictionary<int, Monkey> monkeys;
+        readonly int testTrueMonkeyIndex, testFalseMonkeyIndex;
+        public ulong NumberOfTimesInspected { get; private set; } = 0;
+        public static ulong LeastCommonMultiple { get; set; } = 1;
+
+        public Monkey(IEnumerable<ulong> startingItems, OperationType operation, ulong operationNumber, ulong testNumber, ref Dictionary<int, Monkey> monkeys, int testTrueMonkeyIndex, int testFalseMonkeyIndex) {
+            items = new(startingItems);
+            this.operation = operation;
+            this.operationNumber = operationNumber;
+            TestNumber = testNumber;
+            this.monkeys = monkeys;
+            this.testTrueMonkeyIndex = testTrueMonkeyIndex;
+            this.testFalseMonkeyIndex = testFalseMonkeyIndex;
+        }
+
+        public void PlayTurn(bool part1) {
+            int itemCount = items.Count;
+            if (part1) {
+                for (int itemNumber = 0; itemNumber < itemCount; itemNumber++) {
+
+                    ulong inspectedItem = InspectPart1();
+                    Throw(Test(inspectedItem), inspectedItem);
+                }
+            } else {
+                for (int itemNumber = 0; itemNumber < itemCount; itemNumber++) {
+
+                    ulong inspectedItem = InspectPart2();
+                    Throw(Test(inspectedItem), inspectedItem);
+                }
+            }
+        }
+
+        public void Catch(ulong item) {
+            items.Enqueue(item);
+        }
+
+        private ulong InspectPart1() {
+            NumberOfTimesInspected++;
+            ulong itemToInspect = items.Dequeue();
+
+            switch (operation) {
+                case OperationType.ADD_NUMBER:
+                    return (itemToInspect + operationNumber) / 3;
+                case OperationType.ADD_SELF:
+                    return (itemToInspect + itemToInspect) / 3;
+                case OperationType.MULTIPLY_NUMBER:
+                    return itemToInspect * operationNumber / 3;
+                case OperationType.MULTIPLY_SELF:
+                    return itemToInspect * itemToInspect / 3;
+            }
+            return 0;
+        }
+
+        private ulong InspectPart2() {
+            NumberOfTimesInspected++;
+            ulong itemToInspect = items.Dequeue();
+
+            switch (operation) {
+                case OperationType.ADD_NUMBER:
+                    return (itemToInspect + operationNumber) % LeastCommonMultiple;
+                case OperationType.ADD_SELF:
+                    return (itemToInspect + itemToInspect) % LeastCommonMultiple;
+                case OperationType.MULTIPLY_NUMBER:
+                    return itemToInspect * operationNumber % LeastCommonMultiple;
+                case OperationType.MULTIPLY_SELF:
+                    return itemToInspect * itemToInspect % LeastCommonMultiple;
+            }
+            return 0;
+        }
+
+        private int Test(ulong itemToTest) {
+            return itemToTest % TestNumber == 0 ? testTrueMonkeyIndex : testFalseMonkeyIndex;
+        }
+
+        private void Throw(int catchMonkeyIndex, ulong itemBeingThrown) {
+            monkeys[catchMonkeyIndex].Catch(itemBeingThrown);
+        }
+    }
+
+    enum OperationType {
+        ADD_NUMBER,
+        ADD_SELF,
+        MULTIPLY_NUMBER,
+        MULTIPLY_SELF
+    }
+}
---
+++ 2022/Day11/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Day11 {
+    using System.Text.RegularExpressions;
+    internal class Day11 {
+        static void Main(string[] args) {
+            string[] input = File.ReadAllLines("D:/AdventOfCode/2022/Day11/input.txt");
+
+            //Console.WriteLine(Part1(input));
+            Console.WriteLine(Part2(input));
+        }
+
+        static ulong Part1(string[] input) {
+            Dictionary<int, Monkey> monkeys = new();
+
+            CreateMonkeys(input, ref monkeys);
+
+            for (int round = 0; round < 20; round++) {
+                for (int i = 0; i < monkeys.Count; i++) {
+                    monkeys[i].PlayTurn(true);
+                }
+            }
+
+            KeyValuePair<int,Monkey>[] sortedMonkeys = (from monkey in monkeys orderby monkey.Value.NumberOfTimesInspected descending select monkey).ToArray();
+            return sortedMonkeys[0].Value.NumberOfTimesInspected * sortedMonkeys[1].Value.NumberOfTimesInspected;
+        }
+
+        static ulong Part2(string[] input) {
+            Dictionary<int, Monkey> monkeys = new();
+
+            CreateMonkeys(input, ref monkeys);
+
+            for (int i = 0; i < monkeys.Count; i++) {
+                Monkey.LeastCommonMultiple *= monkeys[i].TestNumber;
+            }
+
+            for (int round = 0; round < 10000; round++) {
+                for (int i = 0; i < monkeys.Count; i++) {
+                    monkeys[i].PlayTurn(false);
+                }
+            }
+
+            KeyValuePair<int, Monkey>[] sortedMonkeys = (from monkey in monkeys orderby monkey.Value.NumberOfTimesInspected descending select monkey).ToArray();
+            return sortedMonkeys[0].Value.NumberOfTimesInspected * sortedMonkeys[1].Value.NumberOfTimesInspected;
+        }
+
+        static void CreateMonkeys(string[] input, ref Dictionary<int,Monkey> monkeys) {
+            int currentMonkey = 0;
+
+            for (int i = 0; i < input.Length; i += 7) {
+
+                IEnumerable<ulong> startingItems = Regex.Matches(input[i + 1], @"\d+").Select(m => ulong.Parse(m.Value));
+                Match numberFoundInOperation = Regex.Match(input[i + 2], @"\d+");
+                OperationType operationType = Regex.Match(input[i + 2], @"[+]").Success
+                        ? numberFoundInOperation.Success ? OperationType.ADD_NUMBER : OperationType.ADD_SELF
+                        : numberFoundInOperation.Success ? OperationType.MULTIPLY_NUMBER : OperationType.MULTIPLY_SELF;
+                ulong operationNumber = numberFoundInOperation.Success ? ulong.Parse(numberFoundInOperation.Value) : 0;
+                ulong testNumber = ulong.Parse(Regex.Match(input[i + 3], @"\d+").Value);
+                int testTrueMonkeyIndex = int.Parse(Regex.Match(input[i + 4], @"\d+").Value);
+                int testFalseMonkeyIndex = int.Parse(Regex.Match(input[i + 5], @"\d+").Value);
+
+                monkeys[currentMonkey++] = new Monkey(
+                    startingItems,
+                    operationType,
+                    operationNumber,
+                    testNumber,
+                    ref monkeys,
+                    testTrueMonkeyIndex,
+                    testFalseMonkeyIndex);
+            }
+        }
+    }
+}
